a8c699434cd5d6cfcd821a0e5301c6cd46fbb1fe
KevinDarnet <kevindarnet.work@gmail.com>
2023-03-27 12:06:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+/* let equipoAcademico = [
+  { nombre: "flor", apellido: "puñales", edad: 40 },
+  { nombre: "kevin", apellido: "darnet", edad: 10 },
+  { nombre: "nico", apellido: "cirulli", edad: 6 },
+  { nombre: "cami", apellido: "domato", edad: 28 },
+  { nombre: "lucre", apellido: "gillone", edad: 27 },
+  { nombre: "guille", apellido: "bonutto", edad: 3 },
+  { nombre: "fede", apellido: "rouyere", edad: 35 },
+];
+let numero = 15;
+
+if (numero < 10) {
+  console.log("Esto es mayor");
+  if (typeof numero == "number") {
+    console.log("Esto es un número");
+  }
+} else {
+  console.log("No es mayor");
+} */
+
+/* if (!equipoAcademico[1].apellido == !"darnet") {
+  console.log("El apellido es " + equipoAcademico[1].apellido);
+} else {
+  console.log("No encontre el mentor");
+} */
+
+// IF = SI ESTO SE CUMPLE HACE ESTO
+// ELSE IF = SINO HACE ESTO OTRO
+// ELSE = NO SE CUMPLE POR ENDE HACES ESTO
+/* if (equipoAcademico[1].edad >= 18 && equipoAcademico[1].nombre == "kevin") {
+  console.log("Esta persona es mayor de edad");
+} else if (
+  equipoAcademico[1].edad < 18 &&
+  equipoAcademico[1].nombre == "kevin"
+) {
+  console.log("Esta persona es menor");
+} else if (!equipoAcademico[1].apellido == "darnet") {
+  console.log("Su apellido coincide con la lista");
+} else {
+  console.log("Esta persona no la podemos identificar");
+}
+ */
+
+/* let frutas = [
+  { nombre: "naranja" },
+  { nombre: "manzana" },
+  { nombre: "pera" },
+  { nombre: "banana" },
+  { nombre: "uva" },
+];
+
+switch (frutas[2].nombre) {
+  case "naranja":
+    console.log("Esta fruta está en stock y vale 2 USD el kilo de naranjas");
+    break;
+  case "manzana":
+    console.log("Esta fruta está en stock y vale 2 USD el kilo de manzanas");
+    break;
+
+  case "pera":
+    console.log("Esta fruta está en stock y vale 2 USD el kilo de peras");
+    break;
+
+  case "banana":
+    console.log("Esta fruta está en stock y vale 2 USD el kilo de bananas");
+    break;
+
+  default:
+    console.log("No tenemos frutas en stock");
+    break;
+}
+ */
